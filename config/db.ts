ffdf5fffc3512af05e2635e99254399762a83095
JuacCrack <jo4quinvel4squez@gmail.com>
2025-05-19 00:10:56 -0300

    fix dbs2

--- config/db.ts
+++ config/db.ts
@@ -1,14 +1,12 @@
 import mysql from 'mysql2/promise';
-import { config } from 'dotenv';
-import { join } from 'path';
-
-config({ path: join(import.meta.dir, 'keys.env') });
 
 export const dbname = process.env.DB_NAME;
 
 export const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: dbname,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true }
 });
