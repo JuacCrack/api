d3cf0a864ac064f1a3259e8b149ee7b37c536467
JuacCrack <jo4quinvel4squez@gmail.com>
2025-05-18 23:50:25 -0300

    fix dbs

--- config/db.ts
+++ config/db.ts
@@ -1,20 +1,14 @@
-import { Database } from 'bun:sqlite'
-import { config } from 'dotenv'
-import { join } from 'path'
+import mysql from 'mysql2/promise';
+import { config } from 'dotenv';
+import { join } from 'path';
 
-// Carga variables de entorno solo si estamos en desarrollo
-if (process.env.NODE_ENV !== 'production') {
-  config({ path: join(import.meta.dir, '../../keys.env') })
-}
+config({ path: join(import.meta.dir, 'keys.env') });
 
-const dbname = process.env.DB_NAME
+export const dbname = process.env.DB_NAME;
 
-if (!dbname) {
-  throw new Error('Falta la variable de entorno DB_NAME para la base de datos SQLite')
-}
-
-const db = new Database(`${dbname}.db`, { create: true })
-
-export function connectDB() {
-  return db
-}
+export const db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: dbname,
+});
